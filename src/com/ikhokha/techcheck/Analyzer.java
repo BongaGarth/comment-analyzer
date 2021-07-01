5eee44a38d4e845e3af4dd59bf9c6fab1f1ade8b
wiredmartian <meggatym@yahoo.com>
2021-07-01 21:26:43 +0200

    working weird

--- src/com/ikhokha/techcheck/Analyzer.java
+++ src/com/ikhokha/techcheck/Analyzer.java
@@ -6,20 +6,54 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class Analyzer {
+public class Analyzer implements Runnable {
+    private final CommentReport report = new CommentReport();
+    private File currentFile;
 
-    public CommentReport analyze() {
-        var report = new CommentReport();
+    public void analyze() {
+        // fixed thread pool
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        // get files
+        Stack<File> files = getCommentFiles();
+        while (files.size() != 0) {
+            currentFile = files.pop();
+            System.out.println(currentFile.getName());
+            executor.execute(this);
+        }
+        executor.shutdown();
         try {
-            // get all files from docs
-            var files = getCommentFiles();
-            while (files.size() != 0) {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private Stack<File> getCommentFiles() {
+        Stack<File> fileStack = new Stack<>();
+        File[] files = new File("docs").listFiles((file, name) -> name.endsWith(".txt"));
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                fileStack.push(file);
+            }
+        }
+        return fileStack;
+    }
+
+    public CommentReport getCurrentReport() {
+        return this.report;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (currentFile != null) {
                 // get file from the stack
-                File cFile = files.pop();
-                BufferedReader reader = new BufferedReader(new FileReader(cFile));
+                BufferedReader reader = new BufferedReader(new FileReader(currentFile));
                 List<String> lines = reader.lines().collect(Collectors.toList());
 
                 // filter the information you want
@@ -39,23 +73,11 @@ public class Analyzer {
                 report.Questions += questions;
                 report.Spam += spam;
 
-                System.out.println("file name: " + cFile.getName());
+                System.out.println("file name: " + currentFile.getName());
+
             }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+        } catch (Exception e)  {
             e.printStackTrace();
         }
-        return report;
-    }
-    private Stack<File> getCommentFiles() {
-        Stack<File> fileStack = new Stack<>();
-        File[] files = new File("docs")
-                .listFiles((file, name) -> name.endsWith(".txt"));
-        if (files != null && files.length > 0) {
-            for (File file : files) {
-                fileStack.push(file);
-            }
-        }
-        return fileStack;
     }
 }
--- src/com/ikhokha/techcheck/Main.java
+++ src/com/ikhokha/techcheck/Main.java
@@ -1,8 +1,15 @@
 package com.ikhokha.techcheck;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
@@ -23,13 +30,39 @@ public class Main {
 		
 		System.out.println("RESULTS\n=======");
 		totalResults.forEach((k,v) -> System.out.println(k + " : " + v));*/
-		Analyzer analyzer = new Analyzer();
+		/* Analyzer analyzer = new Analyzer();
 		CommentReport report = analyzer.analyze();
 		System.out.println("SHORTER_THAN_15: " + report.Shorter);
 		System.out.println("SHAKER_MENTIONS: " + report.Shakers);
 		System.out.println("MOVERS_MENTIONS: " + report.Movers);
 		System.out.println("QUESTIONS: " + report.Questions);
-		System.out.println("SPAM: " + report.Spam);
+		System.out.println("SPAM: " + report.Spam);*/
+		
+		/* ExecutorService executor = Executors.newFixedThreadPool(10);
+		List<CommentReport> reports = Collections.emptyList();
+		File[] files = new File("docs").listFiles((file, s) -> s.endsWith(".txt"));
+		for (File file : files) {
+			var work = new Analyzer(file);
+			executor.execute(work);
+			System.out.println(work.getCurrentReport());
+		}
+		executor.shutdown();*/
+
+		var analyzer = new Analyzer();
+		analyzer.analyze();
+		var report = analyzer.getCurrentReport();
+
+		System.out.println("===================================");
+
+		System.out.printf("SPAM: %s%n", report.Spam);
+		System.out.printf("QUESTIONS: %s%n", report.Questions);
+		System.out.printf("MOVERS_MENTIONS: %s%n", report.Movers);
+		System.out.printf("SHAKERS_MENTIONS: %s%n", report.Shakers);
+		System.out.printf("SHORTER_THAN_15: %s%n", report.Shorter);
+
+		System.out.println("===================================");
+
+
 	}
 	
 	/**
